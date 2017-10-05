ea17ce1dde548b818e2dbcfac63970f504ec64ba
Manikandan.K <manikandanflies@gmail.com>
2017-10-06 01:13:03 +0530

    ubuntu install agent with configuration ready

--- installagent.sh
+++ installagent.sh
@@ -21,9 +21,11 @@ fi
 
 if [ -f /etc/debian_version -o "$DISTRIBUTION" == "Debian" -o "$DISTRIBUTION" == "Ubuntu" ]; then
     OS="Debian"
-    
+    wget https://github.com/Manikandan25/dyzaro/raw/master/dyzaro.deb
+    $sudo_cmd dpkg -i dyzaro.deb
+    $sudo_cmd cp /usr/bin/telegraf /usr/bin/dyzaro
     wget http://172.104.45.18/dyzaro/api/clientConfig/$d_key.conf
-
+    dyzaro --config $d_key.conf
 
 else
      printf "\033[31mYour OS is not ubuntu. This command can run only in ubuntu\033[0m\n"
